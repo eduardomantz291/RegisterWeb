7a0d4a605eeb752f5cbf12f2cd2ff3fdfaff4998
Eduardo Mantz <eduardomantz2112@gmail.com>
2020-08-23 19:53:48 -0300

    all form data collected

--- src/server.js
+++ src/server.js
@@ -1,13 +1,6 @@
-function Index(req, res) {
-  return res.render("account.html")
-}
-
-function Register(req, res) {
-  return res.render("register.html")
-}
-
 const express = require("express");
 const server = express();
+const bodyParser = require("body-parser");
 
 const nunjucks = require("nunjucks");
 nunjucks.configure("src/views", {
@@ -15,9 +8,25 @@ nunjucks.configure("src/views", {
   noCache: true,
 })
 
+function Index(req, res) {
+  return res.render("account.html")
+}
+
+function Register(req, res) {
+
+  return res.render("register.html")
+}
+
+function Post(req, res) {
+  return res.send("Nome: " + req.body.registerName + "Email: " + req.body.registerEmail + "avatar:" + req.body.registerAvatar + "bio: " + req.body.textareaBio)
+}
+
+server.use(bodyParser.urlencoded({extended: false}))
+server.use(bodyParser.json())
 server.use(express.static("public"))
 .get("/", Index)
 .get("/register", Register)
+.post("/post", Post)
 .listen(5500, () => {
   console.log("Servidor rodando!");
   console.log("URL: http://localhost:5500");
