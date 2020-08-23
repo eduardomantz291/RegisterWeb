3a030d3c2a260413037b3bb52ec0900af71ca391
Eduardo Mantz <eduardomantz2112@gmail.com>
2020-08-23 02:08:53 -0300

    all static files loaded with nunjucks.

--- src/server.js
+++ src/server.js
@@ -1,15 +1,24 @@
+function Index(req, res) {
+  return res.render("account.html")
+}
+
+function Register(req, res) {
+  return res.render("register.html")
+}
+
 const express = require("express");
 const server = express();
 
-server.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/account.html");
+const nunjucks = require("nunjucks");
+nunjucks.configure("src/views", {
+  express: server,
+  noCache: true,
 })
 
-server.get("/register", (req, res) => {
-  res.sendFile(__dirname + "/views/register.html");
-})
-
-server.listen(5500, () => {
+server.use(express.static("public"))
+.get("/", Index)
+.get("/register", Register)
+.listen(5500, () => {
   console.log("Servidor rodando!");
   console.log("URL: http://localhost:5500");
 })
