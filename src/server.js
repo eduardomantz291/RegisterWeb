accec454a1d6984cd9c5f4a39c028273672f6afe
Eduardo Mantz <eduardomantz2112@gmail.com>
2020-08-23 01:48:08 -0300

    started the server with express

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const server = express();
+
+server.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/account.html");
+})
+
+server.get("/register.html", (req, res) => {
+  res.sendFile(__dirname + "/views/register.html");
+})
+
+server.listen(5500, () => {
+  console.log("Servidor rodando!");
+  console.log("URL: http://localhost:5500");
+})
